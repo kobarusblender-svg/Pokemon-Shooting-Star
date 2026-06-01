9ae004acbdf947ca91bda0818460a83fc047fac9
trabajospplb <kobarusblender@gmail.com>
2026-06-01 11:11:48 +0200

    Update items.h

--- include/constants/items.h
+++ include/constants/items.h
@@ -720,7 +720,7 @@ enum __attribute__((packed)) Item
     ITEM_KEE_BERRY = 579,
     ITEM_MARANGA_BERRY = 580,*/
     ITEM_ENIGMA_BERRY_E_READER = 568,
-    LAST_BERRY_INDEX =  ITEM_ENIGMA_BERRY_E_READER,
+    LAST_BERRY_INDEX =  ITEM_ENIGMA_BERRY,
 
     // TMs/HMs
     ITEM_TM00 = 581, // MOD CONTEST New TM Item that allows you to forget and remember moves on the go OR that teaches a random compatible move OR that teaches an egg move, etc...
