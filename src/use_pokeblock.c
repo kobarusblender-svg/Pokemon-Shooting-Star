b763b1e1106c80ffb632b4803ae2bf1473f1c799
trabajospplb <kobarusblender@gmail.com>
2026-08-16 12:55:57 +0200

    Undid the sparkle thingy because tacky.
    IDK what i was thinking. I'll just do it another way.

--- src/use_pokeblock.c
+++ src/use_pokeblock.c
@@ -1673,8 +1673,7 @@ static void SpriteCB_SelectionIconCancel(struct Sprite *sprite)
 static void CalculateNumAdditionalSparkles(u8 monIndex)
 {
     u8 sheen = GetMonData(&gPlayerParty[monIndex], MON_DATA_SHEEN);
-    u8 OverflowSheen = GetMonData(&gPlayerParty[monIndex], MON_DATA_OVERFLOWSHEEN);
-    sMenu->numSparkles[sMenu->curLoadId] = GET_NUM_CONDITION_SPARKLES(sheen + OverflowSheen);
+    sMenu->numSparkles[sMenu->curLoadId] = GET_NUM_CONDITION_SPARKLES(sheen);
 }
 
 static void LoadConditionGfx(void)
